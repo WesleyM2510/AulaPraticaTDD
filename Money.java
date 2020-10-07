54e1425294fcdbec55dd1e5b29d11f623ab83251
WesleyM2510 <wesleymaciel1@live.com>
2020-10-07 13:58:12 -0300

    Commit 3

--- Money.java
+++ Money.java
@@ -1,13 +1,14 @@
 abstract class Money  {
 
     protected int amount;
+    protected String currency;
 
     static Money dollar(int amount)  {
-    		return new Dollar(amount);
+    		return new Dollar(amount, "USD");
     	}
 
     	static Money franc(int amount) {
-    		return new Franc(amount);
+    		return new Franc(amount, "CHF");
     	}
 
     public boolean equals(Object object)  {
@@ -16,5 +17,8 @@ abstract class Money  {
     }
 
     abstract Money times(int multiplier);
+      String currency() {
+        return currency;
+      }
 
 }
